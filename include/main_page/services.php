ac5218519ef75e13517056546356db361af04208
nastwik <anastasrassoh@gmail.com>
2025-06-16 12:31:19 +0300

    services creating

---
+++ include/main_page/services.php
@@ -0,0 +1,68 @@
+            <section class="relative overflow-hidden border-top">
+                <div class="container">
+                    <div class="row g-4">
+                        <div class="col-lg-6 offset-lg-3 text-center">
+                            <div class="subtitle wow fadeInUp mb-3">Услуги</div>
+                            <h2 class="wow fadeInUp" data-wow-delay=".2s">Заголовок</h2>
+                            <!-- <p class="lead mb-0 wow fadeInUp">Whether it's a quick refresh or a deep clean transformation, our expert touch leaves your home or office shining.</p> -->
+                            <div class="spacer-single"></div>
+                            <div class="spacer-half"></div>
+                        </div>
+                    </div>
+
+                    <div class="row g-4">
+                        <div class="col-lg-4 col-sm-6">
+                            <div class="relative">
+                                <a href="service-single.html" class="d-block hover">
+                                    <div class="relative overflow-hidden rounded-1 shadow-soft">
+                                        <!-- <img src="images/misc/flowers-crop-3-white.webp" class="w-50 end-0 absolute hover-op-0" alt=""> -->
+                                        <div class="absolute z-2 start-0 w-100 abs-middle fs-36 text-white text-center">
+                                            <span class="btn-main hover-op-1">Узнать больше</span>
+                                        </div>
+                                        <img src="images/services/1.jpg" class="img-fluid hover-scale-1-2" alt="">
+                                        <div class="hover-op-0 abs p-3 px-4 bottom-0 text-center text-light w-100 overlay-black-1 bg-blur">
+                                            <h4>Консультация</h4>
+                                        </div>
+                                    </div>
+                                </a>
+                                
+                            </div>
+                        </div>
+
+                        <div class="col-lg-4 col-sm-6">
+                            <div class="relative">
+                                <a href="service-single.html" class="d-block hover">
+                                    <div class="relative overflow-hidden rounded-1 shadow-soft">
+                                        <!-- <img src="images/misc/flowers-crop-3-white.webp" class="w-50 end-0 absolute hover-op-0" alt=""> -->
+                                        <div class="absolute z-2 start-0 w-100 abs-middle fs-36 text-white text-center">
+                                            <span class="btn-main hover-op-1">Узнать больше</span>
+                                        </div>
+                                        <img src="images/services/2.jpg" class="img-fluid hover-scale-1-2" alt="">
+                                        <div class="hover-op-0 abs p-3 px-4 bottom-0 text-center text-light w-100 overlay-black-1 bg-blur">
+                                            <h4>Исследования</h4>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-4 col-sm-6">
+                            <div class="relative">
+                                <a href="service-single.html" class="d-block hover">
+                                    <div class="relative overflow-hidden rounded-1 shadow-soft">
+                                        <!-- <img src="images/misc/flowers-crop-3-white.webp" class="w-50 end-0 absolute hover-op-0" alt=""> -->
+                                        <div class="absolute z-2 start-0 w-100 abs-middle fs-36 text-white text-center">
+                                            <span class="btn-main hover-op-1">Узнать больше</span>
+                                        </div>
+                                        <img src="images/services/3.jpg" class="img-fluid hover-scale-1-2" alt="">
+                                        <div class="hover-op-0 abs p-3 px-4 bottom-0 text-center text-light w-100 overlay-black-1 bg-blur">
+                                            <h4>Процедуры</h4>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </section>
--- index.php
+++ index.php
@@ -2,4 +2,6 @@
 
    <? require_once 'include/main_page/offer.php' ?>
 
+   <? require_once 'include/main_page/services.php' ?>
+
 <? require_once 'footer.php' ?>

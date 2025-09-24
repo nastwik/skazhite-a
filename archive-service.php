71c475c9e5e8d95d31ed5170d8d12228e408a7fe
Anastasiya <anastasrassoh@gmail.com>
2025-09-24 17:54:21 +0300

    create archive-service.php

--- archive-service.php
+++ archive-service.php
@@ -1,4 +1,4 @@
-<? require_once 'header.php' ?>        
+<?php get_header(); ?>    
    <div class="no-bottom no-top" id="content">
 
       <div id="top"></div>
@@ -24,6 +24,72 @@
             </div>
          </div>
       </section>
+
+                  <?php
+                $service_id = get_the_ID();
+                $terms = get_the_terms($service_id, 'service-categories');
+            ?>
+            
+           <section class="relative overflow-hidden">
+                <div class="container">
+                    <div class="row g-4">
+                        <div class="col-lg-6 offset-lg-3 text-center">
+                            <div class="subtitle wow fadeInUp mb-3">Услуги</div>
+                            <div class="section-title wow fadeInUp" data-wow-delay=".2s">Помощь, которую оказываем</div>
+                            <div class="spacer-single"></div>
+                            <div class="spacer-half"></div>
+                        </div>
+                    </div>
+
+                    <div class="row g-4">
+
+                    <?php
+                        $terms = get_terms(array(
+                            'taxonomy' => 'service-categories',
+                            'hide_empty' => false,
+                        ));
+
+                        if (!empty($terms) && !is_wp_error($terms)) {
+                            foreach ($terms as $term) {
+                            $term_image_id = \Kama\WP_Term_Image::get_image_id($term);
+                            $term_image_url = wp_get_attachment_image_url($term_image_id, 'full'); // Получаем URL изображения
+
+                            if (!$term_image_url) {
+                                continue;
+                            }
+
+                            $term_link = get_term_link($term);
+                    ?>
+                         <div class="col-lg-4 col-sm-6">
+                            <div class="relative shadow-soft">
+                                <a href="<?php echo ($term_link); ?>" class="d-block hover">
+                                    <div class="relative overflow-hidden rounded-1 shadow-soft">
+                                        <div class="absolute z-2 start-0 w-100 abs-middle fs-36 text-white text-center">
+                                            <span class="btn-main hover-op-1">Узнать больше</span>
+                                        </div>
+                                        <div class="services__img"> 
+                                            <img src="<?php echo $term_image_url; ?>" class="img-fluid hover-scale-1-2" alt="">
+                                            <div class="overlay"></div>
+                                        </div>
+                                        <div class="hover-op-0 abs p-3 px-4 bottom-0 text-center text-light w-100 overlay-black-1 bg-blur">
+                                            <div class="item-title"><?php echo ($term->name); ?></div>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+
+                    <?php
+                            }
+                        } else {
+                            echo '<p>Нет доступных категорий.</p>';
+                        }
+                    ?>
+
+                    </div>
+                </div>
+            </section>
+
 
       <section class="relative overflow-hidden">
          <div class="container">
@@ -170,4 +236,4 @@
 
    </div>
 
-<? require_once 'footer.php' ?>
+<?php get_footer(); ?>
